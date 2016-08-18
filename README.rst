c832d0d122b85163db9cf9cff016565beb369dcd
Lorenzo <lorenzo.bertolini@infoporto.it>
2016-08-18 15:16:55 +0200

    django superuser management

--- README.rst
+++ README.rst
@@ -12,7 +12,7 @@ Quick start
 
     INSTALLED_APPS = [
         ...
-        'django-multiimap',
+        'django_multiimap',
     ]
 
 
@@ -23,11 +23,12 @@ Quick start
         'django.contrib.auth.backends.ModelBackend',
     )
 
-3. In project settings, add an 'IMAP_SERVER' dictionary like this::
+3. In project settings, add an 'MULTIIMAP_SERVERS' dictionary like this::
 
-    IMAP_SERVER = {
-        'domain': {'host': 'domain_address', 'group_name': 'the_group_name'},
-        'domain1': {'host': 'domain1_address', 'group_name': 'the_group_name1'}
+    MULTIIMAP_SERVERS = {
+        'domain': {'host': 'domain_address', 'group_name': 'the_group_name', 'superusers': ['address@email.com']},
+        'domain1': {'host': 'domain1_address', 'group_name': 'the_group_name1', 'superusers': []}
     }
 
 This module will create a group with a name like 'multiimap_group_%s' % domain where domain is the @domain.com in user email.
+Users mail addresses in superuser list will be set as django superuser.
--- django_multiimap/backend.py
+++ django_multiimap/backend.py
@@ -2,20 +2,32 @@ from imaplib import IMAP4
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.conf import settings
+from django.db import IntegrityError
 
 
 class Backend:
 
+    def check_superuser(self, domain, user):
+        if settings.MULTIIMAP_SERVERS[domain]['superusers']:
+            if user.username in settings.MULTIIMAP_SERVERS[domain]['superusers']:
+                user.is_staff = True
+                user.is_admin = True
+                user.is_superuser = True
+                try:
+                    user.save()
+                except IntegrityError:
+                    raise IntegrityError
+
     def check_settings(self, domain):
-        if not hasattr(settings, 'IMAP_SERVER'):
-            raise AttributeError('Missing IMAP_SERVER configuration in settings')
-        if not settings.IMAP_SERVER[domain]:
+        if not hasattr(settings, 'MULTIIMAP_SERVERS'):
+            raise AttributeError('Missing MULTIIMAP_SERVERS configuration in settings')
+        if not settings.MULTIIMAP_SERVERS[domain]:
             raise AttributeError('Domain %s not found in settings' % domain)
-        if not settings.IMAP_SERVER[domain]['host']:
+        if not settings.MULTIIMAP_SERVERS[domain]['host']:
             raise AttributeError('Host setting not found in settings for %s' % domain)
 
     def generate_group_name(self, domain):
-        group_name = settings.IMAP_SERVER[domain].get('group_name', domain)
+        group_name = settings.MULTIIMAP_SERVERS[domain].get('group_name', domain)
         return 'multiimap_group_%s' % group_name.replace(' ', '_').lower()
 
     def authenticate(self, username=None, password=None):
@@ -28,7 +40,7 @@ class Backend:
         self.check_settings(domain)
 
         try:
-            address = settings.IMAP_SERVER[domain]['host']
+            address = settings.MULTIIMAP_SERVERS[domain]['host']
             # Check if this user is valid on the mail server
             c = IMAP4(address)
             c.login(username, password)
@@ -40,6 +52,8 @@ class Backend:
         group, created = Group.objects.get_or_create(name=self.generate_group_name(domain))
         user.groups.add(group)
 
+        self.check_superuser(domain, user)
+
         return user
 
     # Required for your backend to work properly - unchanged in most scenarios
@@ -48,5 +62,3 @@ class Backend:
             return get_user_model().objects.get(pk=user_id)
         except get_user_model().DoesNotExist:
             return None
-
-
